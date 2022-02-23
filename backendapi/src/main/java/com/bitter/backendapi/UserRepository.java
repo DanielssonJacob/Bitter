145c2379d781c02d329dedd58be7c16e13a2bd61
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-02-23 16:20:22 +0100

    added UserRepository to backend

---
+++ backendapi/src/main/java/com/bitter/backendapi/UserRepository.java
@@ -0,0 +1,48 @@
+package com.bitter.backendapi;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+    private List<User> users;
+
+    public UserRepository(){
+        users = new ArrayList<>();
+    }
+
+    public User getUserById(Long id) {
+        for (User user : users) {
+            if (user.getId()==(id)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User getUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User addUser(User user){
+        users.add(user);
+        return user;
+    }
+
+    public void deleteUserById(Long id){
+        for (User user : users) {
+            if (user.getId()==id) {
+                users.remove(user);
+                break;
+            }
+        }
+    }
+
+}
